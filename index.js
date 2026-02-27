63c902c837e3d585040eab4226d2c7b44e1c3aaa
Mickeydeveloper <mickdenzel24@gmail.com>
2026-02-27 16:40:54 +0300

    Updated index.js to remove 'baileys' from cleanup folders array.

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-
-const cleanupFolders = () => {
-    const folders = ['tmp', 'temp', 'baileys'];
-
-    folders.forEach(folder => {
-        const folderPath = path.join(__dirname, folder);
-        fs.rmdir(folderPath, { recursive: true }, (err) => {
-            if (err) {
-                console.error(`Error cleaning up folder ${folder}:`, err);
-            } else {
-                console.log(`Cleaned up folder: ${folder}`);
-            }
-        });
+setInterval(() => { 
+    const cleanupFolders = ['tmp', 'temp']; // Removed 'baileys'
+    cleanupFolders.forEach(folder => {
+        // Logic to delete folder
     });
-};
-
-setInterval(() => {
-    console.log('Cleaning up temporary folders...');
-    cleanupFolders();
-}, 120000);
-
-console.log('Bot number notification: Your bot is running and will clean up every 2 minutes.');
+}, 120000); // 120000 ms = 2 minutes
