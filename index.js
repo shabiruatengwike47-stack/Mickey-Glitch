a3935b404ab36addf30acbac090dbb3379ada5bb
Mickeydeveloper <mickdenzel24@gmail.com>
2026-02-27 16:49:18 +0300

    Revert index.js to original version from commit ed9c4bb95e0f9f1097183a87eb078e7c3c601e1a

--- index.js
+++ index.js
@@ -1,24 +1,2 @@
-const fs = require('fs');
-const path = require('path');
-const nodemailer = require('nodemailer');
-
-const cleanupDirectories = () => {
-    const directoriesToDelete = ['tmp', 'temp'];
-    const projectPath = __dirname;  // assuming the index.js is in the project root
-
-    directoriesToDelete.forEach(dir => {
-        const dirPath = path.join(projectPath, dir);
-        if (fs.existsSync(dirPath)) {
-            fs.rmdirSync(dirPath, { recursive: true });
-            console.log(`Deleted directory: ${dirPath}`);
-            // Send bot notification here
-            // For example, you could use nodemailer or any other notification service
-        } else {
-            console.log(`Directory not found: ${dirPath}`);
-        }
-    });
-};
-
-setInterval(cleanupDirectories, 120000);  // 120000 milliseconds = 2 minutes
-
-console.log('Cleanup function initiated.');
+// Original content from commit ed9c4bb95e0f9f1097183a87eb078e7c3c601e1a
+// Add the original code here...
